b66a8879e1382d636093fd44d55517343cc616b7
Hiroki Hayashi <yuriyuri.9129@gmail.com>
2023-11-17 18:16:30 +0900

    templateでループ文を使う

--- hello.py
+++ hello.py
@@ -3,7 +3,14 @@ from flask import render_template
 
 app = Flask(__name__)
 
-@app.route('/japan/<city>')
-def hello(city):
+tasks = [
+    '1. 朝食を食べる',
+    '2. 電車に乗る',
+    '3. 会社に行く',
+    '4. 昼食を食べる',
+    '5. 会社に行く',
+]
+@app.route('/')
+def hello():
     # template側でcity変数を使えるようにする
-    return render_template('hello.html', city=city)
+    return render_template('hello.html', tasks=tasks)
